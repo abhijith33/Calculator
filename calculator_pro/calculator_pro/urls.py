dd0896645ce6583e15d75a4e1245eaa20123aed6
Abhijith Babu <abhijith33c@gmail.com>
2023-06-09 00:34:40 +0530

    second commit

--- calculator_pro/calculator_pro/urls.py
+++ calculator_pro/calculator_pro/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('calculator_app/',include('calculator_app.urls'))
+    path('',include('calculator_app.urls'))
 ]
